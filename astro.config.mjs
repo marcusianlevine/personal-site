9492f0edeb2f2783b7cb35ff16886a3d3575f14f
Marcus Levine <marcusianl@gmail.com>
2026-05-02 11:39:06 +0800

    Switch to theme and add Substack logo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://marcuslevine.com',
+  site: 'https://marcuslevine.com',
+  integrations: [sitemap()],
 });
